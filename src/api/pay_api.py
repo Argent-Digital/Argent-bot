b4c21ef7ec7b18bc0d86d1c3e540605467d60380
Platon <platongimp96@gmail.com>
2026-06-04 14:16:05 +0500

    feat: billing sending route

--- src/api/pay_api.py
+++ src/api/pay_api.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends, Request
+import asyncio
 from src.schemas.pay_schemas import SuccesPay
 from src.schemas.vpn_client_schema import BillingResponse
 from aiogram import Bot
 from src.utils.texts import BotTexts
 from src.auth.dependencies import decode_access_token
 from src.auth.verify_system_token import veify_system_token
+from src.utils.semaphore_sending_notif import send_with_semaphore
 
 router = APIRouter(prefix="/pays", tags=['pays'])
 
@@ -20,29 +22,21 @@ async def succes_pay(user_data: SuccesPay, request: Request, user_id: int = Depe
         return {"status": "ok"}
     except Exception as e:
         print (f"Error pay notif: {e}")
-        return {"status": "error"}
+        return None
     
 @router.post("/warning_users")
 async def billing_notifications(request: Request, notif_data: BillingResponse, service_id: int = Depends(veify_system_token)):
     bot: Bot = request.app.state.bot
-    for user_id in notif_data.deleted_keys:
-        try:
-            await bot.send_message(
-                chat_id=user_id,
-                text=BotTexts.del_key(),
-                parse_mode="html"
-            )
-        except Exception as e:
-            print(f"Error sending del_key to user {user_id}: {e}")
+    try:
+        if notif_data.deleted_keys:
+            tasks_del = [send_with_semaphore(user_id=user_id, text_func=BotTexts.del_key(), bot=bot) for user_id in notif_data.deleted_keys]
+            await asyncio.gather(*tasks_del)
 
-    for user_id in notif_data.user_lower:
-        try:
-            await bot.send_message(
-                chat_id=user_id,
-                text=BotTexts.warning_balance(),
-                parse_mode="html"
-            )
-        except Exception as e:
-            print(f"Error sending warning_balance to user {user_id}: {e}")
+        if notif_data.user_lower:
+            tasks_warn = [send_with_semaphore(user_id=user_id, text_func=BotTexts.warning_balance(), bot=bot) for user_id in notif_data.user_lower]
+            await asyncio.gather(*tasks_warn)
 
-    return {"status": "ok"}
+        return {"status": "ok"}
+    except Exception as e:
+        print (f"Error sending bill notif: {e}")
+        return None
---
+++ src/utils/semaphore_sending_notif.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiogram import Bot
+
+semaphore = asyncio.Semaphore(20)
+
+
+async def send_with_semaphore(user_id: int, text_func: str, bot: Bot):
+    async with semaphore:
+        try:
+            await bot.send_message(
+                    chat_id=user_id,
+                    text=text_func,
+                    parse_mode="html"
+                )
+        except Exception as e:
+            print(f"Error sending to user {user_id}: {e}")
