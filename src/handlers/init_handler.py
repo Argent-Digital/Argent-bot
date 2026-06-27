d7a294872e35a63471e57cccca6cc156e9ab6793
platon <platongimp96@gmail.com>
2026-06-27 21:29:07 +0500

    feat: adm router in dispatcher

--- src/handlers/init_handler.py
+++ src/handlers/init_handler.py
@@ -2,12 +2,14 @@ from aiogram import Router
 from src.handlers.base_handler import router as base_router
 from src.handlers.key_handler import router as key_router
 from src.handlers.pay_handler import router as pay_router
+from src.handlers.adm_handler import router as adm_router
 
 main_router = Router()
 
 main_router.include_router(base_router)
 main_router.include_router(key_router)
 main_router.include_router(pay_router)
+main_router.include_router(adm_router)
 
 def get_main_router() -> Router:
     return main_router
